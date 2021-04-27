daadd7dac8e21c04b77033d07ac054c672add3b1
bella <bella@ifchic.com>
2021-04-27 11:17:37 +0800

    Install necessary plugin

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+    'jest': true
+  },
+  'plugins': ['react'],
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
+    'react/display-name': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+  }
+}
